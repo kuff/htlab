9799d7844fa1ae19c30bef72f9b75e9f4d1dd9af
kuff <16867891+kuff@users.noreply.github.com>
2022-10-24 23:02:08 +0200

    Add customizable startup state and KeyCodes to TrackingManager

--- Assets/Scripts/TrackingManager.cs
+++ Assets/Scripts/TrackingManager.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using Leap;
 using Leap.Unity;
 using UnityEngine;
 
@@ -9,54 +9,69 @@ using UnityEngine;
  */
 public class TrackingManager : MonoBehaviour
 {
-    private enum TrackingProvider
+    public enum TrackingProvider
     {
         Ultraleap,
         Oculus,
         Both
     }
+
+    [Tooltip("TrackingProvider to use on app startup.")]
+    public TrackingProvider initialProvider;
+
+    [Tooltip("KeyCode for enabling the Ultraleap hand tracking provider.")]
+    public KeyCode ultraleapKeyCode;
+    [Tooltip("KeyCode for enabling the Oculus hand tracking provider.")]
+    public KeyCode oculusKeyCode;
+    [Tooltip("KeyCode for enabling both Ultraleap and Oculus hand tracking providers simultaneously.")]
+    public KeyCode bothKeyCode;
     
     private GameObject[] _ultraleapObjects;
     private GameObject[] _oculusObjects;
 
     protected void Start()
     {
+        // Check validity of KeyCodes
+        var keyCodeSet = new HashSet<KeyCode>
+        {
+            ultraleapKeyCode,
+            oculusKeyCode,
+            bothKeyCode
+        };
+        if (keyCodeSet.Count != 3)
+            Debug.LogError("Overlapping KeyCode values given");
+
+        // Find and save object(s) related to each hand tracking provider
         _ultraleapObjects   = new GameObject[1];
         _oculusObjects      = new GameObject[2];
         
-        // Find and save object(s) related to each hand tracking provider
         // Ultraleap
         var localLeapProvider = FindObjectOfType<LeapXRServiceProvider>()?.gameObject;
         if (localLeapProvider is null) 
-            Debug.LogError("Unable to locate Ultraleap GameObjects");
+            Debug.LogError("Unable to locate Ultraleap GameObjects in scene");
         else 
             _ultraleapObjects[0] = localLeapProvider;
         
         // Oculus
         var localOVRProvider = FindObjectsOfType<OVRHand>();
         if (localOVRProvider is null || localOVRProvider.Length != 2)
-            Debug.LogError("Unable to locate Oculus GameObjects");
+            Debug.LogError("Unable to locate Oculus GameObjects in scene");
         else
             _oculusObjects = localOVRProvider.Select(hand => hand.gameObject).ToArray();
+        
+        // Set initial provider
+        ChangeTrackingProvider(initialProvider);
     }
 
     protected void Update()
     {
         // Listen for key presses and change hand tracking provider accordingly
-        if (Input.GetKeyDown("b"))
+        if (Input.GetKeyDown(ultraleapKeyCode))
             ChangeTrackingProvider(TrackingProvider.Ultraleap);
-        else if (Input.GetKeyDown("n"))
+        else if (Input.GetKeyDown(oculusKeyCode))
             ChangeTrackingProvider(TrackingProvider.Oculus);
-        else if (Input.GetKeyDown("m"))
+        else if (Input.GetKeyDown(bothKeyCode))
             ChangeTrackingProvider(TrackingProvider.Both);
-        
-        // Run continuous check to Ultraleap hands until both are found
-        /*if (_ultraleapObjects[0] == null)
-        {
-            var localLeapProvider = FindObjectOfType<LeapXRServiceProvider>()?.gameObject;
-            if (localLeapProvider != null) return;
-            _ultraleapObjects[1] = localLeapProvider;
-        }*/
     }
 
     private void ChangeTrackingProvider(TrackingProvider tp)
@@ -80,7 +95,7 @@ public class TrackingManager : MonoBehaviour
                 foreach (var go in _ultraleapObjects)   go.SetActive(true);
                 break;
             default:
-                throw new ArgumentOutOfRangeException(nameof(tp), tp, "Unsupported Tracking Provider given");
+                throw new ArgumentOutOfRangeException(nameof(tp), tp, "Unsupported TrackingProvider given");
         }
     }
 }
